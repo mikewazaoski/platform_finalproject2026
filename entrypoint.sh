000a394b0f6abd787028fcbbbfde1d57d7a7872e
mikewazaoski <mikemarco97@gmail.com>
2026-05-16 15:32:16 +0800

    5 files added

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+entrypoint.sh
+
+#!/bin/bash
+set -e
+
+echo "Starting PHP-FPM..."
+php-fpm -F &
+PHP_PID=$!
+
+echo "Waiting for PHP-FPM to start..."
+sleep 2
+
+echo "Starting Nginx..."
+nginx -g "daemon off;"
+
+wait $PHP_PID
